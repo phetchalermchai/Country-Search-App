7715dbfc04f05faa423d920d831e3acbcf144d14
unknown <phetangelb@gmail.com>
2022-12-02 23:02:02 +0700

    add project

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import {useState,useEffect} from "react"
+function App() {
+  
+  const [countries,setCountries] = useState([])
+  const [word,setWord] = useState("")
+  const [dataFilter] = useState(["name","capital"]) 
+
+
+
+  useEffect(()=>{
+    fetch("https://restcountries.com/v2/all")
+    .then(res=>res.json())
+    .then(data=>{
+      setCountries(data)
+    })
+  },[])
+
+  const formatNumber=(num)=> {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+  const searchCountries=(countriess)=>{
+    return countriess.filter((items)=>{
+        return dataFilter.some((filters)=>{
+            if(items[filters]){
+              return items[filters].toString().toLowerCase().indexOf(word.toLowerCase())>-1
+            }
+        })
+    })
+}
+
+  return (
+    <div className='container'>
+        <div className='search-container'>
+            <label className="search-form">
+              <input type="text" className='search-input' placeholder='ค้นหาข้อมูลประเทศที่คุณต้องการ (เมืองหลวง,ชื่อประเทศ)' value={word} onChange={(e)=>setWord(e.target.value)}/>
+            </label>
+        </div>
+      <ul className='row'>
+        {searchCountries(countries).map((item,index)=>{
+          return(
+              <li key={index}>
+                  <div className='card'>
+                      <div className='card-title'>
+                          <img src={item.flag} alt={item.name}/>      
+                      </div>
+                      <div className='card-body'>
+                          <div className='card-description'>
+                              <h2>{item.name}</h2>
+                              <ol className='card-list'>
+                                  <li>ประชาการ : <span>{formatNumber(item.population)}</span></li>
+                                  <li>ภูมิภาค : <span>{item.region}</span></li>
+                                  <li>เมืองหลวง : <span>{item.capital}</span></li>
+                              </ol>
+                          </div>
+                      </div>
+                  </div>
+              </li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
